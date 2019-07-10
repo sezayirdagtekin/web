fae1d6c3d5b06c520dfc2d693365a9bef1087215
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2019-07-10 08:19:47 +0300

    prompt example

--- basicWeb/scriptFile.js
+++ basicWeb/scriptFile.js
@@ -3,3 +3,12 @@
 function showDate(){
     document.getElementById('demo').innerHTML=Date();
 }
+
+
+function promptUser(){
+    var name= window.prompt("enter your name");
+    if(name!=null && name!=""){
+        document.getElementById("name").innerHTML="Hello "+name;
+    }
+  
+}
