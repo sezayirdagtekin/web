91fcfd553fc8a790a79120990bff0b25d4679407
sezayirdagtekin <sezayir@hotmail.com>
2019-10-06 10:49:30 +0300

    create 'Employee table component' and register App.vue

--- basicWeb/vujs/vue-employee/src/App.vue
+++ basicWeb/vujs/vue-employee/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Employees</h1>
+    <employe-table-component />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import EmployeeTable from '@/components/EmployeeTable.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,EmployeeTable
   }
 }
 </script>
---
+++ basicWeb/vujs/vue-employee/src/components/EmployeeTable.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="employe-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Richard Hendricks</td>
+          <td>richard@piedpiper.com</td>
+        </tr>
+        <tr>
+          <td>Bertram Gilfoyle</td>
+          <td>gilfoyle@piedpiper.com</td>
+        </tr>
+        <tr>
+          <td>Dinesh Chugtai</td>
+          <td>dinesh@piedpiper.com</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+
+<script>
+   export default{
+      name:"employe-table-component"
+    }
+</script>
+
+
+<style scoped></style>
