843e6ec60ed121269573f9a9ea3bf6021f467960
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2019-10-17 12:31:59 +0300

    add book list component

---
+++ basicWeb/vujs/vue-book/src/components/BookList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookList",
+
+  data() {
+    return {
+      title: "All Books"
+    };
+  }
+};
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+</style>
