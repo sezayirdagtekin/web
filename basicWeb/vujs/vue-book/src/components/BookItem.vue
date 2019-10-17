21b955dc696a1a97d988cacb0bdc5a565974af51
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2019-10-17 16:45:52 +0300

    remove ul add tables

--- basicWeb/vujs/vue-book/src/components/BookItem.vue
+++ basicWeb/vujs/vue-book/src/components/BookItem.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <li>{{book.title}}:{{book.author}}</li>
+    <td>{{book.title}}</td>
+    <td>{{book.author}}</td>
   </div>
 </template>
 
@@ -12,8 +13,33 @@ export default {
 </script>
 
 <style scoped>
-li {
-  display: block;
-  margin: 0 10px;
+
+ table {
+    border: 2px solid #42b983;
+    border-radius: 3px;
+    background-color: #fff;
+  }
+  
+  th {
+    background-color: #42b983;
+    color: rgba(255,255,255,0.66);
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+  
+  td {
+    background-color: #f9f9f9;
+  }
+
+    div {
+      
+    padding: 5px;
+    margin: 5px;
 }
+
+
+
 </style>
--- basicWeb/vujs/vue-book/src/components/BookList.vue
+++ basicWeb/vujs/vue-book/src/components/BookList.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>
+  <div align="center">
     <h1>{{title}}</h1>
-    <ul>
-      <book-form @addBook="appendBook"></book-form>
-      <hr />
-      <book-item v-for="book in books" :book="book"></book-item>
-    </ul>
+
+    <book-form @addBook="appendBook"></book-form>
+    <hr />
+    <table>
+        <thead>
+          <tr>
+          <th>Name</th>
+          <th>Author</th>     
+        </tr>
+      </thead>
+      <tbody>
+      <tr v-for="book in books">
+        <book-item :book="book"></book-item>
+      </tr>
+     </tbody>
+    </table>
   </div>
 </template>
 
@@ -39,21 +50,36 @@ export default {
 </script>
 
 <style scoped>
+
+  
+table {
+    border: 2px solid #42b983;
+    border-radius: 3px;
+    background-color: #fff;
+  }
+  
+  th {
+    background-color: #42b983;
+    color: rgba(255,255,255,0.66);
+    cursor: pointer;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+  
+  td {
+    background-color: #f9f9f9;
+  }
+  
+  th, td {
+    min-width: 120px;
+    padding: 10px 20px;
+  }
+
 h1,
 h2 {
   font-weight: normal;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: block;
-  margin: 0 10px;
-}
-div {
-  padding: 5px;
-  margin: 5px;
-}
 </style>
