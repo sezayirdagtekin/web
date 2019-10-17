2bd6b48b5f42fbc6778af130d43832f324b7eeed
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2019-10-17 14:01:57 +0300

    create and wire child component

--- basicWeb/vujs/vue-book/src/components/BookList.vue
+++ basicWeb/vujs/vue-book/src/components/BookList.vue
@@ -2,15 +2,22 @@
   <div>
     <h1>{{title}}</h1>
     <ul>
-      <li v-for="book in books"> {{book.title}}:{{book.author}}</li>
+      <book-item v-for='book in books' :book='book'></book-item>
 
     </ul>
   </div>
 </template>
 
 <script>
+
+import BookItem from './BookItem'
+
 export default {
   name: "BookList",
+  components: {
+    BookItem
+  },
+
 
   data() {
     return {
