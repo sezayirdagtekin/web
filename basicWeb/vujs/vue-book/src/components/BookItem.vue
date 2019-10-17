009994f2f464f014dcdac1bd9c1b9f72c61d3fce
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2019-10-17 17:22:12 +0300

    refactor

--- basicWeb/vujs/vue-book/src/components/BookItem.vue
+++ basicWeb/vujs/vue-book/src/components/BookItem.vue
@@ -13,33 +13,14 @@ export default {
 </script>
 
 <style scoped>
+td {
+  padding: 2px;
+  margin: 2px;
 
- table {
-    border: 2px solid #42b983;
-    border-radius: 3px;
-    background-color: #fff;
-  }
-  
-  th {
-    background-color: #42b983;
-    color: rgba(255,255,255,0.66);
-    cursor: pointer;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-  }
-  
-  td {
-    background-color: #f9f9f9;
-  }
-
-    div {
-      
-    padding: 5px;
-    margin: 5px;
 }
 
-
-
+div {
+  padding: 5px;
+  margin: 5px;
+}
 </style>
--- basicWeb/vujs/vue-book/src/components/BookList.vue
+++ basicWeb/vujs/vue-book/src/components/BookList.vue
@@ -5,17 +5,9 @@
     <book-form @addBook="appendBook"></book-form>
     <hr />
     <table>
-        <thead>
-          <tr>
-          <th>Name</th>
-          <th>Author</th>     
-        </tr>
-      </thead>
-      <tbody>
       <tr v-for="book in books">
         <book-item :book="book"></book-item>
       </tr>
-     </tbody>
     </table>
   </div>
 </template>
@@ -51,34 +43,15 @@ export default {
 
 <style scoped>
 
-  
-table {
+  table {
     border: 2px solid #42b983;
     border-radius: 3px;
-    background-color: #fff;
-  }
-  
-  th {
-    background-color: #42b983;
-    color: rgba(255,255,255,0.66);
-    cursor: pointer;
-    -webkit-user-select: none;
-    -moz-user-select: none;
-    -ms-user-select: none;
-    user-select: none;
-  }
-  
-  td {
-    background-color: #f9f9f9;
-  }
-  
-  th, td {
-    min-width: 120px;
-    padding: 10px 20px;
+    background-color: #fff;  
   }
 
-h1,
-h2 {
+
+
+h1,h2 {
   font-weight: normal;
 }
 
