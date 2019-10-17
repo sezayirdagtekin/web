0d6437179f2f28d3d26aeca2d4f72590fe94abcf
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2019-10-17 13:37:28 +0300

    create child component

---
+++ basicWeb/vujs/vue-book/src/components/BookItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <li>{{book.title}}:{{book.author}}</li>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookItem",
+  props: ["book"]
+};
+</script>
+
+<style scoped>
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
