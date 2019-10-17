4bccf759b85a167f7c5e75f92bb8148c58f515ba
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2019-10-17 13:07:27 +0300

    dynamic list

--- basicWeb/vujs/vue-book/src/components/BookList.vue
+++ basicWeb/vujs/vue-book/src/components/BookList.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <ul>
+      <li v-for="book in books"> {{book.title}} -  {{book.author}}</li>
+
+    </ul>
   </div>
 </template>
 
@@ -10,7 +14,12 @@ export default {
 
   data() {
     return {
-      title: "All Books"
+      title: "All Books",
+      books: [
+        { title: "Anna Karenina", author: "Leo Tolstoy" },
+        { title: "Madame Bovary", author: "Gustave Flaubert" },
+        { title: "The Stories of Anton Chekhov", author: "Anton Chekhov" }
+      ]
     };
   }
 };
@@ -20,5 +29,15 @@ export default {
 h1,
 h2 {
   font-weight: normal;
+}
+
+ul {
+  list-style: none;
+  padding: 5px;
+  margin: 5px;
+}
+li {
+  display: block;
+  margin: 0 10px;
 }
 </style>
