566805674b5e772b3137e69f186ff67ced6da210
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2019-10-22 15:45:27 +0300

    add links

--- basicWeb/vujs/vue-getting-started/03-data-and-events/begin/vue-heroes/src/components/header-bar-links.vue
+++ basicWeb/vujs/vue-getting-started/03-data-and-events/begin/vue-heroes/src/components/header-bar-links.vue
@@ -4,14 +4,14 @@
       <div class="navbar-item">
         <div class="buttons">
           <a
-            href="https://github.com/johnpapa/vue-getting-started"
+            :href="github"
             target="_blank"
             rel="noopener noreferrer"
           >
             <i class="fab fa-github fa-2x" aria-hidden="true"></i>
           </a>
           <a
-            href="https://twitter.com/john_papa"
+            :href="twitter"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -22,3 +22,20 @@
     </div>
   </div>
 </template>
+
+<script>
+  export default {
+    name:"HeaderBarLinks",
+    data() {
+      return {
+        github: 'https://github.com/sezayirdagtekin/',
+        twitter:'https://twitter.com/sezayirdagtekin'
+      }
+    },
+    
+  }
+</script>
+
+<style  scoped>
+
+</style>
