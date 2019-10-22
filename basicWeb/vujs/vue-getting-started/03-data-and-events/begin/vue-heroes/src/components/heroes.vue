92a81622af053b7bb7a7aa5db808f56482d0d7b5
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2019-10-22 16:42:58 +0300

    v-bind style

--- basicWeb/vujs/vue-getting-started/03-data-and-events/begin/vue-heroes/src/components/heroes.vue
+++ basicWeb/vujs/vue-getting-started/03-data-and-events/begin/vue-heroes/src/components/heroes.vue
@@ -30,18 +30,18 @@
               <div class="field">
                 <label class="label">cape color</label>
                 <label class="radio" for="color-red">
-                  <input type="radio" id="color-red" value="red" />
+                  <input type="radio" id="color-red" value="red"  v-model="hero.capeColor"/>
                   red
                 </label>
                 <label class="radio" for="color-blue">
-                  <input type="radio" id="color-blue" value="blue" />
+                  <input type="radio" id="color-blue" value="blue"  v-model="hero.capeColor" />
                   blue
                 </label>
                 <label class="radio" for="color-green">
-                  <input type="radio" id="color-green" value="green" />
+                  <input type="radio" id="color-green" value="green"  v-model="hero.capeColor" />
                   green
                 </label>
-                <div class="color-line"></div>
+                <div class="color-line" v-bind:style ="{'background-color': hero.capeColor}"></div>
               </div>
               <div class="field">
                 <label for="power">
@@ -60,7 +60,7 @@
               <div class="field">
                 <label class="checkbox" for="active">
                   active
-                  <input type="checkbox" class="is-primary" id="active" />
+                  <input type="checkbox" class="is-primary" id="active" v-model="hero.active" />
                 </label>
               </div>
             </div>
