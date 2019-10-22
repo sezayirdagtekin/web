f6d96d7e3947a86d9cd6fe0cd4a4bb3c4baa296e
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2019-10-22 16:17:12 +0300

    click event

--- basicWeb/vujs/vue-getting-started/03-data-and-events/begin/vue-heroes/src/components/heroes.vue
+++ basicWeb/vujs/vue-getting-started/03-data-and-events/begin/vue-heroes/src/components/heroes.vue
@@ -7,13 +7,13 @@
       <div class="column is-8">
         <div class="card edit-detail">
           <header class="card-header">
-            <p class="card-header-title"></p>
+            <p class="card-header-title">{{hero.firstName}}</p>
           </header>
           <div class="card-content">
             <div class="content">
               <div class="field">
                 <label class="label" for="id">id</label>
-                <label class="input" id="id" readonly></label>
+                <label class="input" id="id" readonly>{{hero.id}}</label>
               </div>
               <div class="field">
                 <label class="label" for="firstName">first name</label>
@@ -66,17 +66,17 @@
             </div>
           </div>
           <footer class="card-footer">
-            <button class="link card-footer-item cancel-button">
+            <button class="link card-footer-item cancel-button" @click="cancelHero">
               <i class="fas fa-undo"></i>
               <span>Cancel</span>
             </button>
-            <button class="link card-footer-item">
+            <button class="link card-footer-item" @click="saveHero">
               <i class="fas fa-save"></i>
               <span>Save</span>
             </button>
           </footer>
         </div>
-        <div class="notification is-info"></div>
+        <div class="notification is-info"><pre>{{message}}</pre></div>
       </div>
     </div>
   </div>
@@ -85,5 +85,31 @@
 <script>
 export default {
   name: 'Heroes',
+  data() {
+    return {
+      hero:{
+        id:20,
+        firstName:'Sezayir',
+        lastname:'Dagtekin',
+        description:'the cat whisperere',
+        capeColor:'',
+        power:'',
+        active:true,
+      },
+      message:'',
+    }
+  },
+methods: {
+
+  cancelHero() {
+      this.message='';
+  },
+
+
+  saveHero(){
+    this.message=JSON.stringify(this.hero,null,'\n');
+  }
+},
+
 };
 </script>
