da35785450e6214d7f2c23ac49dda3ced9d7b7c5
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2019-10-24 12:15:53 +0300

    refactor one component in to two component

--- basicWeb/vujs/vue-getting-started/06-component-communication/begin/vue-heroes/src/components/hero-detail.vue
+++ basicWeb/vujs/vue-getting-started/06-component-communication/begin/vue-heroes/src/components/hero-detail.vue
@@ -1,22 +1,130 @@
 <template>
-    <div>
-<p> hellooooooooooooooooooooooooooooooooooooooo {{ hero.firstName}}</p>
+  <div>
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">{{ fullName }}</p>
+      </header>
+      <div class="card-content">
+        <div class="content">
+          <div class="field">
+            <label class="label" for="id">id</label>
+            <label class="input" name="id" readonly>
+              {{
+              hero.id
+              }}
+            </label>
+          </div>
+          <div class="field">
+            <label class="label" for="firstName">first name</label>
+            <input class="input" name="firstName" v-model="hero.firstName" />
+          </div>
+          <div class="field">
+            <label class="label" for="lastName">last name</label>
+            <input class="input" name="lastName" v-model="hero.lastName" />
+          </div>
+          <div class="field">
+            <label class="label" for="description">description</label>
+            <input class="input" name="description" v-model="hero.description" />
+          </div>
+          <div class="field">
+            <label class="label" for="originDate">origin date</label>
+            <input type="date" class="input" id="originDate" v-model="hero.originDate" />
+            <p class="comment">
+              My origin story began on
+              {{ hero.originDate | shortDate }}
+            </p>
+          </div>
+          <div class="field">
+            <label class="label" for="capeCounter">cape counter</label>
+            <input class="input" name="capeCounter" type="number" v-model="hero.capeCounter" />
+          </div>
+          <div class="field">
+            <label class="label" for="capeMessage">cape message</label>
+            <label class="input" name="capeMessage">
+              {{
+              capeMessage
+              }}
+            </label>
+          </div>
+        </div>
+      </div>
+      <footer class="card-footer">
+        <button class="link card-footer-item cancel-button" @click="cancelHero()">
+          <i class="fas fa-undo"></i>
+          <span>Cancel</span>
+        </button>
+        <button class="link card-footer-item" @click="saveHero()">
+          <i class="fas fa-save"></i>
+          <span>Save</span>
+        </button>
+      </footer>
     </div>
+  </div>
 </template>
 
-<script>
-    export default {
-        name: 'HeroDetail',
 
-        props: {
-          hero: {
-                type: Object,
-                default: () => {}
-            },
-        },
-    }
+<script>
+import { format } from 'date-fns';
+import { displayDateFormat } from '../shared';
+
+export default {
+  name: 'HeroDetail',
+
+  props: {
+    hero: {
+      type: Object,
+      default: () => {},
+    },
+  },
+
+  computed: {
+    fullName() {
+      return this.hero ? `${this.hero.firstName} ${this.hero.lastName}` : '';
+    },
+  },
+
+  methods: {
+    cancelHero() {
+      //placeholder
+    },
+    saveHero() {},
+    handleTheCapes(newValue) {
+      const value = parseInt(newValue, 10);
+      switch (value) {
+        case 0:
+          this.capeMessage = 'Where is my cape?';
+          break;
+        case 1:
+          this.capeMessage = 'One is all I need';
+          break;
+        case 2:
+          this.capeMessage = 'Alway have a spare';
+          break;
+        default:
+          this.capeMessage = 'You can never have enough capes';
+          break;
+      }
+    },
+  },
+
+  watch: {
+    'selectedHero.capeCounter': {
+      immediate: true,
+      handler(newValue, oldValue) {
+        console.log(
+          `CapeCounter watcher evalauted. old=${oldValue}, new=${newValue}`,
+        );
+        this.handleTheCapes(newValue);
+      },
+    },
+  },
+  filters: {
+    shortDate: function(value) {
+      return format(value, displayDateFormat);
+    },
+  },
+};
 </script>
 <style scoped>
-
 </style>
 
--- basicWeb/vujs/vue-getting-started/06-component-communication/begin/vue-heroes/src/components/heroes.vue
+++ basicWeb/vujs/vue-getting-started/06-component-communication/begin/vue-heroes/src/components/heroes.vue
@@ -33,88 +33,7 @@
           <!-- <hero-detail> -->
           <HeroDetail v-else-if="selectedHero" :hero="selectedHero"></HeroDetail>
     
-          <div v-if="selectedHero">
-            <div class="card">
-              <header class="card-header">
-                <p class="card-header-title">{{ fullName }}</p>
-              </header>
-              <div class="card-content">
-                <div class="content">
-                  <div class="field">
-                    <label class="label" for="id">id</label>
-                    <label class="input" name="id" readonly>{{
-                      selectedHero.id
-                    }}</label>
-                  </div>
-                  <div class="field">
-                    <label class="label" for="firstName">first name</label>
-                    <input
-                      class="input"
-                      name="firstName"
-                      v-model="selectedHero.firstName"
-                    />
-                  </div>
-                  <div class="field">
-                    <label class="label" for="lastName">last name</label>
-                    <input
-                      class="input"
-                      name="lastName"
-                      v-model="selectedHero.lastName"
-                    />
-                  </div>
-                  <div class="field">
-                    <label class="label" for="description">description</label>
-                    <input
-                      class="input"
-                      name="description"
-                      v-model="selectedHero.description"
-                    />
-                  </div>
-                  <div class="field">
-                    <label class="label" for="originDate">origin date</label>
-                    <input
-                      type="date"
-                      class="input"
-                      id="originDate"
-                      v-model="selectedHero.originDate"
-                    />
-                    <p class="comment">
-                      My origin story began on
-                      {{ selectedHero.originDate | shortDate }}
-                    </p>
-                  </div>
-                  <div class="field">
-                    <label class="label" for="capeCounter">cape counter</label>
-                    <input
-                      class="input"
-                      name="capeCounter"
-                      type="number"
-                      v-model="selectedHero.capeCounter"
-                    />
-                  </div>
-                  <div class="field">
-                    <label class="label" for="capeMessage">cape message</label>
-                    <label class="input" name="capeMessage">{{
-                      capeMessage
-                    }}</label>
-                  </div>
-                </div>
-              </div>
-              <footer class="card-footer">
-                <button
-                  class="link card-footer-item cancel-button"
-                  @click="cancelHero()"
-                >
-                  <i class="fas fa-undo"></i>
-                  <span>Cancel</span>
-                </button>
-                <button class="link card-footer-item" @click="saveHero()">
-                  <i class="fas fa-save"></i>
-                  <span>Save</span>
-                </button>
-              </footer>
-            </div>
-          </div>
+        
           <!-- </hero-detail> -->
 
           <div class="notification is-info" v-show="message">{{ message }}</div>
@@ -125,10 +44,8 @@
 </template>
 
 <script>
-import { format } from 'date-fns';
+import { ourHeroes } from '../shared';
 import HeroDetail from '@/components/hero-detail';
-
-import { displayDateFormat, ourHeroes } from '../shared';
 
 export default {
   name: 'Heroes',
@@ -146,13 +63,7 @@ export default {
   created() {
     this.loadHeroes();
   },
-  computed: {
-    fullName() {
-      return this.selectedHero
-        ? `${this.selectedHero.firstName} ${this.selectedHero.lastName}`
-        : '';
-    },
-  },
+
   methods: {
     async getHeroes() {
       return new Promise(resolve => {
@@ -177,39 +88,8 @@ export default {
     selectHero(hero) {
       this.selectedHero = hero;
     },
-    handleTheCapes(newValue) {
-      const value = parseInt(newValue, 10);
-      switch (value) {
-        case 0:
-          this.capeMessage = 'Where is my cape?';
-          break;
-        case 1:
-          this.capeMessage = 'One is all I need';
-          break;
-        case 2:
-          this.capeMessage = 'Alway have a spare';
-          break;
-        default:
-          this.capeMessage = 'You can never have enough capes';
-          break;
-      }
-    },
+  
   },
-  watch: {
-    'selectedHero.capeCounter': {
-      immediate: true,
-      handler(newValue, oldValue) {
-        console.log(
-          `CapeCounter watcher evalauted. old=${oldValue}, new=${newValue}`
-        );
-        this.handleTheCapes(newValue);
-      },
-    },
-  },
-  filters: {
-    shortDate: function(value) {
-      return format(value, displayDateFormat);
-    },
-  },
+
 };
 </script>
