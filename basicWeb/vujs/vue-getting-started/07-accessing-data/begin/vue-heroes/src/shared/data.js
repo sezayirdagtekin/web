6cab3a7de089870b21df3c20da1d92aa8d5687a4
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2019-10-25 08:50:29 +0300

    getting data from different environment

--- basicWeb/vujs/vue-getting-started/07-accessing-data/begin/vue-heroes/src/shared/data.js
+++ basicWeb/vujs/vue-getting-started/07-accessing-data/begin/vue-heroes/src/shared/data.js
@@ -1,10 +1,11 @@
 import { format } from 'date-fns';
 import { inputDateFormat } from './constants';
 import * as axios from 'axios';
+import { API } from './config';
 
 const getHeroes = async function() {
   try {
-    const response = await axios.get('api/heroes.json');
+    const response = await axios.get(`${API}/heroes.json`);
 
     const heroes = response.data.map(h => {
       h.originDate = format(h.originDate, inputDateFormat);
--- basicWeb/vujs/vue-getting-started/07-accessing-data/begin/vue-heroes/src/shared/index.js
+++ basicWeb/vujs/vue-getting-started/07-accessing-data/begin/vue-heroes/src/shared/index.js
@@ -1,3 +1,4 @@
+export * from './config';
 export * from './constants';
 export * from './data';
 export * from './logger';
