6659480d745bb1bbaaabb000c9dd8bc630e52a30
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2020-02-11 18:15:28 +0300

    use property adn  send data parent to chield

--- vujs/training/ch8/cominication-problem/src/components/User.vue
+++ vujs/training/ch8/cominication-problem/src/components/User.vue
@@ -2,10 +2,11 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeName"> Change my name</button>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail v-bind:name="name"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -19,6 +20,16 @@
     import UserEdit from './UserEdit.vue';
 
     export default {
+        data() {
+            return {
+                name: 'sezayir'
+            }
+        },
+        methods: {
+            changeName() {
+                this.name='Anna'
+            }
+        },
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
--- vujs/training/ch8/cominication-problem/src/components/UserDetail.vue
+++ vujs/training/ch8/cominication-problem/src/components/UserDetail.vue
@@ -2,10 +2,15 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User name: {{name}} </p>
     </div>
 </template>
 
 <script>
+
+export default{
+    props:['name']
+}
 </script>
 
 <style scoped>
