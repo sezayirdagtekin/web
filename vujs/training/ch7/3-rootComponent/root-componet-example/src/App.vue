10eb4f46b967ffb4870239903d59d97ee0bef6fb
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2020-02-11 10:41:07 +0300

    root component

---
+++ vujs/training/ch7/3-rootComponent/root-componet-example/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1> Server status: {{status}} </h1>   
+    <h1> Ip:{{ip}}</h1>   
+  </div>
+</template>
+
+<script>
+export default{
+  data: function(){
+      return {
+       status:'Critical',
+       ip:'192.10.23.54'
+      }
+    }
+}
+
+</script>
+
+<style>
+
+</style>
