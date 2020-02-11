20862dee29337315e10369dac2811cd904ce9e94
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2020-02-11 18:41:45 +0300

    add method get return   reverse name and display in  detail component

--- vujs/training/ch8/cominication-problem/src/components/UserDetail.vue
+++ vujs/training/ch8/cominication-problem/src/components/UserDetail.vue
@@ -3,13 +3,19 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User name: {{myName}} </p>
+        <p>Reverse name: {{  reverseName()}} </p>
     </div>
 </template>
 
 <script>
 
 export default{
-    props:['myName']
+    props:['myName'],
+    methods:{
+        reverseName(){
+            return this.myName.split("").reverse().join("");
+        }
+    }
 }
 </script>
 
