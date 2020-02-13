08231bf01e934b5714f6e6c16fdb34901730208a
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2020-02-13 17:25:24 +0300

    dynamic conten

--- vujs/training/ch9/1-advanced-component/src/App.vue
+++ vujs/training/ch9/1-advanced-component/src/App.vue
@@ -2,10 +2,21 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
+<button @click="selectedComponent='appQuote'" >Quote</button>
+<button @click="selectedComponent='appAuthor'">Author</button>
+<button @click="selectedComponent='appNew'">New</button>
+<hr>
+<p> {{selectedComponent}}</p>
+<!-- //1. simple slot example. To see please open
         <app-quote>
         <h2 slot="title"> {{title}}</h2>
         <p> That is wonderful</p>
          </app-quote>
+ -->
+    <component :is="selectedComponent">
+        <p> Default content</p>
+    </component>
+
       </div>
     </div>
   </div>
@@ -13,15 +24,21 @@
 
 <script>
 import Quote from "./component/Quote.vue";
+import Author from './component/Author.vue';
+import New from './component/New.vue';
 
 export default {
   components: {
-    appQuote: Quote
+    appQuote: Quote,
+    appAuthor:Author,
+    appNew:New
   },
 
   data() {
     return {
-      title: "This is slot example!"
+      title: "This is slot example!",
+      selectedComponent:'appQuote'
+
     };
   }
 };
---
+++ vujs/training/ch9/1-advanced-component/src/component/New.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>The New Quote</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
