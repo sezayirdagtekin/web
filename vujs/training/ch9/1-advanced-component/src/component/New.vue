7972f01b1392f10291d8286297060107480d7ef3
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2020-02-13 18:13:57 +0300

    use create and destroy metheod see component life cycle

--- vujs/training/ch9/1-advanced-component/src/component/New.vue
+++ vujs/training/ch9/1-advanced-component/src/component/New.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
     <p>The New Quote</p>
+    <button @click="counter++">Increment</button>
+    <p>{{ counter }}</p>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      counter: 0
+    };
+  },
+    created() {
+      console.log("Created..");
+  },
+  destroyed() {
+      console.log("Destroyed..");
+  }
+};
 </script>
 
 <style scoped></style>
