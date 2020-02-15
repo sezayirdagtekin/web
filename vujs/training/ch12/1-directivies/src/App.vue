79466acc722af9ad0306195747d74711820d7e1e
sezayirdagtekin <sezayir@hotmail.com>
2020-02-15 13:39:56 +0300

    create custom diective

--- vujs/training/ch12/1-directivies/src/App.vue
+++ vujs/training/ch12/1-directivies/src/App.vue
@@ -5,9 +5,16 @@
                 <h1>Directives</h1>
                 <p v-text="'Some text'"></p>
                 <p v-html="'<strong>Some html </strong>'"> </p>
-
             </div>
         </div>
+
+         <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custome Directives</h1>
+                 <p v-highlight>some custome directive</p>
+            </div>
+        </div>
+
     </div>
 </template>
 
