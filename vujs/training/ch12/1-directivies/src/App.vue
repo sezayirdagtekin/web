793793fc83f15ec843a0d18add6624532ff431a5
sezayirdagtekin <sezayir@hotmail.com>
2020-02-15 14:16:49 +0300

    add paramater as binding

--- vujs/training/ch12/1-directivies/src/App.vue
+++ vujs/training/ch12/1-directivies/src/App.vue
@@ -11,7 +11,7 @@
          <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custome Directives</h1>
-                 <p v-highlight>some custome directive</p>
+                 <p v-highlight="'red'">some custome directive</p>
             </div>
         </div>
 
