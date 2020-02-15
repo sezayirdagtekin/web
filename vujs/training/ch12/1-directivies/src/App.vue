8e7c3d926caaa586c12c13e20e412ee9d64baa7e
sezayirdagtekin <sezayir@hotmail.com>
2020-02-15 14:37:34 +0300

    add delay as modifier

--- vujs/training/ch12/1-directivies/src/App.vue
+++ vujs/training/ch12/1-directivies/src/App.vue
@@ -12,7 +12,9 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Custome Directives</h1>
                  <p v-highlight:background="'green'">some custome directive one</p>
-                 <p v-highlight="'red'">some custome directive second </p>
+                  <p v-highlight:background.delayed="'red'">some custome directive second</p>
+                 <p v-highlight="'red'">some custome directive third </p>
+              
             </div>
         </div>
 
--- vujs/training/ch12/1-directivies/src/main.js
+++ vujs/training/ch12/1-directivies/src/main.js
@@ -1,16 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.directive('highlight',{
-  bind(el,binding,vnode){
+Vue.directive('highlight', {
+  bind(el, binding, vnode) {
     //el.style.backgroundColor='green'
     //el.style.backgroundColor=binding.value;
-    if(binding.arg=='background'){
-      el.style.backgroundColor=binding.value;
+    let delay = 0;
+    if (binding.modifiers['delayed']) {
+        delay = 3000;
     }
-    else{
-      el.style.color=binding.value;
-    }
+
+    setTimeout(() => {
+      change(el, binding);
+    }, delay);
+
 
   }
 });
@@ -19,3 +22,14 @@ new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+
+function change (el, binding) {
+  if (binding.arg == 'background') {
+    el.style.backgroundColor = binding.value;
+  }
+  else {
+    el.style.color = binding.value;
+  }
+}
+
