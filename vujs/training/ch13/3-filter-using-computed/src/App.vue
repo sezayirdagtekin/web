f314a36ab12e7a25594d587613117b12226bfa8c
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2020-02-20 12:13:59 +0300

    use computed property  filter array list

---
+++ vujs/training/ch13/3-filter-using-computed/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Filters & Mixins</h1>
+        <hr />
+        <input type="text" v-model="filterText" />
+        <ul>
+          <li v-for="fruit in filterFruits" :key="fruit">
+            {{ fruit }}
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fruits: ["Apple", "Banana", "Mango", "Melon"],
+      filterText: ""
+    };
+  },
+  computed: {
+    filterFruits() {
+      return this.fruits.filter(element => {
+        return element.match(this.filterText);
+      });
+    }
+  }
+};
+</script>
+
+<style></style>
