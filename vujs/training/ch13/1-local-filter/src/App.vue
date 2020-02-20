9f7107664a477cf43821345a42c8577c156b1bd4
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2020-02-20 10:36:13 +0300

    simple local filter

---
+++ vujs/training/ch13/1-local-filter/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Simple filter 1</h1>
+        <p>{{ text | toUpperCase }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: "Hello vue.js filter"
+    };
+  },
+  filters: {
+    toUpperCase(value) {
+      return value.toUpperCase();
+    }
+  }
+};
+</script>
+
+<style></style>
