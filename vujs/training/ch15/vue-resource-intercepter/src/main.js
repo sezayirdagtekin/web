4e59d7b9440dbd08d1350620f93094e52804944f
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2020-02-21 15:01:22 +0300

    interceptor

--- vujs/training/ch15/vue-resource-intercepter/src/main.js
+++ vujs/training/ch15/vue-resource-intercepter/src/main.js
@@ -4,7 +4,7 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
-Vue.http.options.root='https://vuejs-http-2e327.firebaseio.com/data.json'//chnage this url with actual firebase database url
+Vue.http.options.root='dummyUrl'//chnage this url with actual firebase database url
 Vue.http.interceptors.push((request,next)=>{
   console.log("Interceptor catched request:"+request);
   next();
