ad7f8f4524bfc641809db6c834260d995d1d0c27
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2020-02-21 12:13:52 +0300

    vujs using resources using fire base

---
+++ vujs/training/ch15/vue-resource-using-firebase/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Http</h1>
+        <div class="form-group">
+          <label>Username</label>
+          <input class="form-control" type="text" v-model="user.name" />
+        </div>
+        <div class="form-group">
+          <label>Email</label>
+          <input class="form-control" type="text" v-model="user.email" />
+        </div>
+        <button class="btn btn-primary" @click="submit">Submit</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: "",
+        email: ""
+      }
+    };
+  },
+  methods: {
+    submit() {
+      this.$http
+        .post("dummyUrl.firebaseio.com/data.json", this.user)
+        .then(
+          reponse => {
+            console.log(reponse);
+          },
+          error => {
+            console.log(error);
+          }
+        );
+    }
+  }
+};
+</script>
+
+<style></style>
