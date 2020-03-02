2d62dd3ce06b6d261d7ae63f5425979d4b97f8d7
sezayir Dagtekin <sezayir.dagtekin@ge.com>
2020-03-02 15:16:23 +0300

    two way three faz1

---
+++ vujs/two-way-tree/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-data></app-data>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Data from "./components/Data.vue";
+
+export default {
+  created() {
+    console.log("created");
+  },
+
+  components: {
+    appData: Data
+  }
+};
+</script>
+
+<style>
+div.component {
+  border: 1px solid black;
+  padding: 30px;
+}
+</style>
---
+++ vujs/two-way-tree/src/components/Data.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="categoryHierarchy">
+    
+    <ul>
+         <li v-for="parent in parents" :key="parent.name"> {{parent.name}} </li>
+    </ul>
+<hr>
+{{myTree}}
+<!--
+     <ul>
+         <li v-for="child in children" :key="child.name"> {{child.name}} </li>
+    </ul>
+    -->
+    
+  </div>
+
+
+
+</template>
+
+<script>
+import $ from 'jquery'
+import { FruitMixin } from "./../mixin/mixin";
+
+export default {
+
+  mixins: [FruitMixin],
+  created() {
+    console.log("created");
+  }
+
+
+}
+
+</script>
+
+<style></style>
